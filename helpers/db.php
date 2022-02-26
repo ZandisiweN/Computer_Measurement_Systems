718083d5c0f149a585d83db5832e2d639d987021
Fortune Zviregei <fortunechainz@gmail.com>
2022-02-26 16:17:20 +0100

    Made changes

--- helpers/db.php
+++ helpers/db.php
@@ -1,15 +1,30 @@
 <!-- database connection -->
 <?php
-$servername = "45.87.80.175";
-$username = "u733134329_zandisiwendhlo";
-$password = "oUXhHCa0?qC";
+$host = '45.87.80.175:3306';
+$db = 'u733134329_mydatabase';
+$user = 'u733134329_zandisiwendhlo';
+$pass = 'oUXhHCa0?qC';
+$port = "3306";
+$charset = 'utf8mb4';
+// connect to the database $conn=mysqli_connect($host, $user, $pass, $db); // check connection if
+$conn = mysqli_connect($host, $user, $pass, $db);
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password);
-
-// Check connection
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+  echo 'Connection error: ' . mysqli_connect_error();
 }
-echo "Connected successfully";
-?>
+
+// write query for all pizzas
+// $sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
+$sql = 'SELECT * FROM hosts';
+
+// // get the result set (set of rows)
+$result = mysqli_query($conn, $sql);
+
+// // fetch the resulting rows as an array
+$hosts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// // free the $result from memory (good practise)
+mysqli_free_result($result);
+
+
+mysqli_close($conn);
--- z1/task2.php
+++ z1/task2.php
@@ -1,5 +1,31 @@
 <?php include '../partials/header.php' ?>
-<?php include '../helpers/check_status.php' ?>
+<?php include '../helpers/db.php' ?>
+
+<div class="container">
+  <h1 class="text-center mt-3 mb-5">List of hosts</h1>
+  <table class="table table-hover table-warning">
+    <thead class="table-primary">
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">HOST</th>
+        <th scope="col">STATUS</th>
+        <th scope="col">ATTEMPTS</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php ?>
+      <?php foreach ($hosts as $i => $host) : ?>
+      <tr class="table-active">
+        <th scope="row"><?php echo $host['id'] ?></th>
+        <td><?php echo $host['address'] ?></td>
+        <td class="address"><?php $host['address'] ?></td>
+        <td class="attempts">0</td>
+      </tr>
+      <?php endforeach  ?>
+    </tbody>
+  </table>
+</div>
+
 
 
 <?php include '../partials/footer.php' ?>
